5acf9715a8c3d9ff47834181c068bc11c99c2fea
James Titcumb <james@asgrim.com>
2015-08-25 08:54:32 +0100

    Apply transformation to scene with matrices

--- hhvm_opengl.cpp
+++ hhvm_opengl.cpp
@@ -4,7 +4,13 @@
 #include <stdio.h>
 #include <GL/glew.h>
 #include <GL/glfw.h>
+
+#ifndef GLM_FORCE_RADIANS
+#define GLM_FORCE_RADIANS
+#endif
+
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
 
 #include "lib/shader.hpp"
@@ -23,13 +29,24 @@ namespace HPHP {
         void sweep() {
         }
 
+        int width;
+        int height;
+
         GLuint programID;
         GLuint vertexPosition_modelspaceID;
         GLuint vertexBuffer;
+        GLuint MatrixID;
+        glm::mat4 MVP;
+        glm::mat4 Model;
+        glm::mat4 View;
+        glm::mat4 Projection;
     };
 
     void HHVM_METHOD(OpenGL, __construct) {
         auto data = Native::data<OpenGL>(this_);
+
+        data->width = 1024;
+        data->height = 768;
 
         if (!glfwInit()) {
             throw Object(SystemLib::AllocExceptionObject("Unable to init GLFW"));
@@ -38,14 +55,24 @@ namespace HPHP {
         glfwOpenWindowHint(GLFW_FSAA_SAMPLES, 4);
         glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 2);
         glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 1);
-        glfwOpenWindow(800, 600, 1, 1, 1, 0, 32, 0, GLFW_WINDOW);
+        glfwOpenWindow(data->width, data->height, 1, 1, 1, 0, 32, 0, GLFW_WINDOW);
         glewInit();
         glfwSetWindowTitle( "Hello world" );
         glfwEnable( GLFW_STICKY_KEYS );
         glClearColor(0.0f, 0.0f, 0.9f, 0.0f);
 
         data->programID = LoadShaders( "shaders/SimpleVertexShader.vertexshader", "shaders/SimpleFragmentShader.fragmentshader" );
+        data->MatrixID = glGetUniformLocation(data->programID, "MVP");
         data->vertexPosition_modelspaceID = glGetAttribLocation(data->programID, "vertexPosition_modelspace");
+
+        data->Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+        data->View = glm::lookAt(
+            glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
+            glm::vec3(0,0,0), // and looks at the origin
+            glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+        );
+        data->Model = glm::mat4(1.0f);
+        data->MVP = data->Projection * data->View * data->Model;
 
         static const GLfloat g_vertex_buffer_data[] = {
                 -1.0f, -1.0f, 0.0f,
@@ -63,6 +90,9 @@ namespace HPHP {
 
         glClear( GL_COLOR_BUFFER_BIT );
         glUseProgram(data->programID);
+
+        glUniformMatrix4fv(data->MatrixID, 1, GL_FALSE, &data->MVP[0][0]);
+
         glEnableVertexAttribArray(data->vertexPosition_modelspaceID);
         glBindBuffer(GL_ARRAY_BUFFER, data->vertexBuffer);
         glVertexAttribPointer(
@@ -83,17 +113,23 @@ namespace HPHP {
     }
 
     String HHVM_METHOD(OpenGL, getBackBufferContent) {
-        GLubyte *data = (GLubyte*)malloc(2 * 800 * 600 * sizeof(GLubyte));
+        auto data = Native::data<OpenGL>(this_);
+
+        GLubyte *pixels = (GLubyte*)malloc(2 * data->width * data->height * sizeof(GLubyte));
         glReadBuffer(GL_BACK);
-        glReadPixels(0, 0, 800, 600, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glReadPixels(0, 0, data->width, data->height, GL_RGB, GL_UNSIGNED_BYTE, pixels);
 
         String filename = String("/tmp/lol.bmp");
-        WriteBMP(filename.c_str(), 800, 600, data);
+        WriteBMP(filename.c_str(), data->width, data->height, pixels);
 
         return filename;
     }
 
     void HHVM_METHOD(OpenGL, close) {
+        auto data = Native::data<OpenGL>(this_);
+
+        glDeleteBuffers(1, &data->vertexBuffer);
+        glDeleteProgram(data->programID);
         glfwTerminate();
     }
 
