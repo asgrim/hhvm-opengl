1dda19411bc1c18bba0adf0507c028b1843f65a9
James Titcumb <james@asgrim.com>
2016-05-19 10:23:26 +0100

    Added faux-animation to test script

--- test.php
+++ test.php
@@ -1,13 +1,15 @@
 <?php
 
+$x = (float)($_GET['f'] ?: 1);
+
 $gl = new OpenGL(1024, 768);
 
 // Set the scene
 $redGrey = new OpenGLColour(195, 179, 179);
 $scenery = new OpenGLPolygon([
-    new OpenGLCuboid(new OpenGLVertex(0.9, -1, 0), 1, 1, 1),
-    new OpenGLCuboid(new OpenGLVertex(-2, -1, -0.5), 2, 2, 2),
-    new OpenGLCuboid(new OpenGLVertex(-7, -1, -1), 3, 3, 3),
+    new OpenGLCuboid(new OpenGLVertex(0.9, -1, 0 * $x), 1, 1, 1),
+    new OpenGLCuboid(new OpenGLVertex(-2, -1, -0.5 * $x), 2, 2, 2),
+    new OpenGLCuboid(new OpenGLVertex(-7, -1, -1 * $x), 3, 3, 3),
     new OpenGLPolygon([
         new OpenGLTriangle(new OpenGLVertex(-20, -1, -20, $redGrey), new OpenGLVertex(10, -1, 10, $redGrey), new OpenGLVertex(10, -1, -20, $redGrey)),
         new OpenGLTriangle(new OpenGLVertex(-20, -1, -20, $redGrey), new OpenGLVertex(-20, -1, 10, $redGrey), new OpenGLVertex(10, -1, 10, $redGrey)),
@@ -27,11 +29,15 @@ $gl->setColourBuffer($scenery->getColourBuffer());
 // Render the scene
 $gl->render();
 
+// Purposefully prevent rendering too quickly (we get black screens and too much flicker in animation)
+usleep(200000);
+
 // Grab the scene
 $f = $gl->getBackBufferContent();
 $c = file_get_contents($f);
 $gl->close();
 
 // Display the scene
+header('Refresh:0; url=test.php?f=' . ($x + 0.3));
 header('Content-Type:image/bmp');
 echo $c;
